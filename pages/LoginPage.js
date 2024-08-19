b359247fbba57b2249f1b23f91fd99f6e5d6eeec
jedi24garcia <jedi24garcia@gmail.com>
2024-08-19 15:12:02 +1200

    added bg image

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
+import { StyleSheet, Text, ImageBackground, image, TouchableOpacity, View, Alert } from 'react-native';
+import { SocialIcon } from 'react-native-elements';
 
 const EntryPage = () => {
     return (
         <View style={styles.container}>
+            <ImageBackground source={require('../images/cool_city.jpg')} style={styles.image}>
+            <Text style={styles.authenticate}>Email Address</Text>
+            <Text style={styles.authenticate}>Password</Text>
             <TouchableOpacity style={styles.SignInButton} onPress={() => Alert.alert('Button Pressed')}>
                 <Text style={styles.SignInFont}>Login</Text>
             </TouchableOpacity>
+            <Text style={styles.authenticate}>Login with Social Accounts</Text>
+            <View style={styles.SocialMedia}>
+                <SocialIcon type="facebook" />
+                <SocialIcon type="twitter" />
+                <SocialIcon type="google" />
+            </View>
+            </ImageBackground>
         </View>
     );   
 };
@@ -15,20 +26,33 @@ export default EntryPage;
 
 const styles = StyleSheet.create ({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center',
         flex: 1,
+        flexDirection: 'column',
+    },
+    image: {
+        resizeMode: 'cover',
+        flex: 1,
+        justifyContent: 'center',
+    },
+    authenticate: {
+        padding: 20,
+        textAlign: 'center',
+        color: 'white',
     },
     SignInButton: {
         backgroundColor: '#00d3ff',
         borderRadius: 100,
         width: '70%',
         marginBottom: 10,
-        textAlign: 'center',
         padding: 15,
+        textAlign: 'center',
     },
     SignInFont: {
         color: '#F5F5F5',
         textAlign: 'center',
+    },
+    SocialMedia: {
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
 })
