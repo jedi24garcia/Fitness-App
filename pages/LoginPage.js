a9a1a394a6c8dc2c8de682baf5c132123df12b1a
jedi24garcia <jedi24garcia@gmail.com>
2024-08-19 12:17:10 +1200

    modified loginpage

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const EntryPage = () => {
     return (
         <View>
-            <Text>Hello</Text>
+            <Text style={styles.SignInFont}>Login</Text>
         </View>
     );   
 };
 
 export default EntryPage;
+
+const styles = StyleSheet.create ({
+    SignInFont: {
+        color: '#F5F5F5',
+        textAlign: 'center',
+    }
+})
