4d952f1cd24dca4be1e37978d0c714f87b620c77
jedi24garcia <jedi24garcia@gmail.com>
2024-08-19 20:38:05 +1200

    partially completed login page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, ImageBackground, image, TouchableOpacity, View, Alert } from 'react-native';
+import { StyleSheet, Text, ImageBackground, TouchableOpacity, View, Alert, TextInput } from 'react-native';
 import { SocialIcon } from 'react-native-elements';
 
 const EntryPage = () => {
+    const [text, setText] = React.useState('');
     return (
         <View style={styles.container}>
-            <ImageBackground source={require('../images/cool_city.jpg')} style={styles.image}>
+            <ImageBackground source={require('../images/gym.jpg')} style={styles.image}>
             <Text style={styles.authenticate}>Email Address</Text>
+            <TextInput style={styles.input} placeholder="Type email address here" newText={newText => setText(newText)} defaultValue={text} />
             <Text style={styles.authenticate}>Password</Text>
-            <TouchableOpacity style={styles.SignInButton} onPress={() => Alert.alert('Button Pressed')}>
+            <TextInput style={styles.input} placeholder="Type password address here" newText={newText => setText(newText)} defaultValue={text} />
+            <View style={styles.SignInButton}>
+            <TouchableOpacity onPress={() => Alert.alert('Button Pressed')}>
                 <Text style={styles.SignInFont}>Login</Text>
             </TouchableOpacity>
+            </View>
             <Text style={styles.authenticate}>Login with Social Accounts</Text>
             <View style={styles.SocialMedia}>
                 <SocialIcon type="facebook" />
@@ -28,14 +33,25 @@ const styles = StyleSheet.create ({
     container: {
         flex: 1,
         flexDirection: 'column',
+        opacity: 0.7,
+        backgroundColor: '#083236',
     },
     image: {
         resizeMode: 'cover',
         flex: 1,
         justifyContent: 'center',
     },
+    input: {
+        textAlign: 'center',
+        color: 'white',
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 100,
+        padding: 10,  
+    },
     authenticate: {
-        padding: 20,
+        padding: 50,
         textAlign: 'center',
         color: 'white',
     },
@@ -45,7 +61,7 @@ const styles = StyleSheet.create ({
         width: '70%',
         marginBottom: 10,
         padding: 15,
-        textAlign: 'center',
+        alignSelf: 'center',
     },
     SignInFont: {
         color: '#F5F5F5',
