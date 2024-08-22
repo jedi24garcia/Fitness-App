ee0470c7fd130b4d6e5ecf1350bc2025feba04bc
jedi24garcia <jedi24garcia@gmail.com>
2024-08-23 11:00:30 +1200

    updated opening page

--- pages/OpeningPage.js
+++ pages/OpeningPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 const OpenPage = () => {
     return (
-        <Text>Hello</Text>
+        <View style={styles.container}>
+            <Image style={styles.fitnessImage} source={require('../images/gym.jpg')} />
+        <Text style={styles.welcomeText}>Hello</Text>
+        </View>
     );
 };
 
 export default OpenPage;
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: 'black',
+    },
+    welcomeText: {
+        color: 'whitesmoke',
+    },
+    fitnessImage: {
+        width: 370,
+        height: 350,
+        marginBottom: 40,
+        borderRadius: 10,  
+    },
+})
