19bc76195baa8deb7b847de1a323579cf6c0f4f7
jedi24garcia <jedi24garcia@gmail.com>
2024-08-23 14:44:47 +1200

    somehow fixed

--- pages/OpeningPage.js
+++ pages/OpeningPage.js
@@ -1,32 +1,24 @@
 import React, { useRef } from 'react';
-import { StyleSheet, View, Text, Image, FlatList, Animated } from 'react-native';
+import { StyleSheet, View, Text, Image, Animated, ScrollView, Dimensions } from 'react-native';
 import { ExpandingDot } from 'react-native-animated-pagination-dots';
+
+const { width: screenWidth } = Dimensions.get('window');
 
 const OpenPage = ({ navigation }) => {
     const scrollX = useRef(new Animated.Value(0)).current;
-    const data = [1]
 
     const handleScrollEnd = (event) => {
         const contentOffsetX = event.nativeEvent.contentOffset.x;
-        const currentPage = Math.round(contentOffsetX / styles.fitnessImage.width);
+        const currentPage = Math.round(contentOffsetX / screenWidth);
 
-        if (currentPage === data.length -1) {
+        if (currentPage === 1) { 
             navigation.navigate('Entry');
         }
     };
 
-    const renderItem = () => (
-        <View style={styles.container}>
-            <Image style={styles.fitnessImage} source={require('../images/gym.jpg')} />
-            <Text style={styles.welcomeText}>Hello</Text>
-        </View>
-    );
-
     return (
         <View style={styles.container}>
-            <FlatList
-                data={data}
-                renderItem={renderItem}
+            <ScrollView
                 horizontal
                 pagingEnabled
                 showsHorizontalScrollIndicator={false}
@@ -37,9 +29,19 @@ const OpenPage = ({ navigation }) => {
                 scrollEventThrottle={16}
                 keyExtractor={(item, index) => index.toString()}
                 onMomentumScrollEnd={handleScrollEnd}
-            />
+            >
+                <View style={styles.page}>
+                    <Image style={styles.fitnessImage} source={require('../images/gym.jpg')} />
+                    <Text style={styles.welcomeText}>Hello</Text>
+                </View>
+                <View style={styles.page} />
+            </ScrollView>
 
-            <ExpandingDot data={data} scrollX={scrollX} expandingDotWidth={30} inActiveDotColor='rgba(255, 255, 255, 0.6)'
+            <ExpandingDot 
+                data={[1, 2]} 
+                scrollX={scrollX} 
+                expandingDotWidth={30} 
+                inActiveDotColor='rgba(255, 255, 255, 0.6)'
                 dotStyle={{
                     width: 10,
                     height: 10,
@@ -48,7 +50,8 @@ const OpenPage = ({ navigation }) => {
                     marginHorizontal: 5,
                 }}
                 containerStyle={{
-                    top: 30,
+                    position: 'absolute',
+                    bottom: 30,
                 }} 
             />
         </View>
@@ -60,17 +63,20 @@ export default OpenPage;
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        justifyContent: 'center', 
-        alignItems: 'center', 
         backgroundColor: 'black',
+    },
+    page: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: screenWidth,
     },
     welcomeText: {
         color: 'whitesmoke',
+        marginTop: 20,
     },
     fitnessImage: {
         width: 370,
         height: 350,
-        marginBottom: 40,
         borderRadius: 10,  
     },
-})
+});
