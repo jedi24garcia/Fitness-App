2e1abb50c8a7d513d00582aea6750d326dad6c6c
jedi24garcia <jedi24garcia@gmail.com>
2024-08-24 14:00:58 +1200

    modified signup page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, ImageBackground, TouchableOpacity, View, Alert, TextInput } from 'react-native';
 import { SocialIcon } from 'react-native-elements';
 
-const EntryPage = () => {
+const EntryPage = ({ navigation }) => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [emailPlaceholder, setEmailPlaceholder] = React.useState('Type email address here');
     const [passwordPlaceholder, setPasswordPlaceholder] = React.useState('Type password here');
     return (
         <View style={styles.container}>
-            <ImageBackground source={require('../images/gym.jpg')} style={styles.image} imageStyle={{ opacity: 0.7 }}>
+            {/* <ImageBackground source={require('../images/gym.jpg')} style={styles.image} imageStyle={{ opacity: 0.7 }}> */}
                 <View style={styles.content}>
                     <Text style={styles.authenticate}>Email Address</Text>
                     <TextInput style={styles.input} placeholder={emailPlaceholder} value={email} onChangeText={newText => setEmail(newText)} onFocus={() => setEmailPlaceholder('')} onBlur={() => setEmailPlaceholder('Type email address here')} />
@@ -22,7 +22,7 @@ const EntryPage = () => {
                     </View>
                     <Text style={styles.authenticate}>Don't have an account?</Text>
                     <View style={styles.authenticateButton}>
-                        <TouchableOpacity onPress={() => Alert.alert('Signed Up')}>
+                        <TouchableOpacity onPress={() => navigation.navigate('Sign')}>
                             <Text style={styles.authenticateFont}>Create Account</Text>
                         </TouchableOpacity>
                     </View>
@@ -33,7 +33,7 @@ const EntryPage = () => {
                         <SocialIcon type="google" />
                     </View>
                 </View>
-            </ImageBackground>
+            {/* </ImageBackground> */}
         </View>
     );   
 };
@@ -43,13 +43,12 @@ export default EntryPage;
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        // backgroundColor: '#083236',
+        backgroundColor: '#083236',
     },
     content: {
         flex: 1,
         resizeMode: 'cover',
-        padding: 20,
-        marginBottom: '50%',
+        padding: 100,
     },
     image: {
         resizeMode: 'cover',
@@ -59,11 +58,11 @@ const styles = StyleSheet.create ({
     input: {
         textAlign: 'center',
         color: 'white',
-        height: 40,
+        height: 50,
         margin: 12,
-        borderWidth: 1,
-        borderRadius: 100,
-        padding: 10,  
+        borderWidth: 5,
+        borderRadius: 100, 
+        marginBottom: 30,
     },
     authenticate: {
         padding: 10,
--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 const SignUpPage = () => {
+    const [text, newText] = React.useState('');
     return (
-        <View styles={style.container}>
-            <Text>Create Account</Text>
+        <View style={styles.container}>
+            <View style={styles.signContent}>
+                <Text style={styles.SignUpFont}>Create Account</Text>
+            </View>
+            <View>
+                <Text style={styles.fonts}>Name</Text>
+                <TextInput style={styles.input} onChangeText={newText => (newText)} value={text} />
+            </View>
+            <View>
+                <Text style={styles.fonts}>Email</Text>
+                <TextInput style={styles.input} onChangeText={newText => (newText)} value={text} />
+            </View>
+            <View>
+                <Text style={styles.fonts}>Password</Text>
+                <TextInput style={styles.input} onChangeText={newText => (newText)} value={text} />
+            </View>
+            <View>
+                <Text style={styles.fonts}>Confirm Password</Text>
+                <TextInput style={styles.input} onChangeText={newText => (newText)} value={text} />
+            </View>
         </View>
     );
 };
 
 export default SignUpPage;
 
-const style = StyleSheet.create ({
+const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        bbackgroundColor: '#083236',
+        backgroundColor: '#083236',
+        padding: 100,
+    },
+    signContent: {
+        marginBottom: 20,
+    },
+    SignUpFont: {
+        color: '#00d3ff',
+        textAlign: 'center',
+        fontSize: 30,
+    },
+    fonts: {
+        textAlign: 'center',
+    },
+    input: {
+        textAlign: 'center',
+        color: 'white',
+        height: 50,
+        margin: 12,
+        borderWidth: 5,
+        borderRadius: 100, 
     },
 })
