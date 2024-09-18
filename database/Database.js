b116796b10e128453ec6c51e3fcbb28f43a373c6
jedi24garcia <jedi24garcia@gmail.com>
2024-09-18 19:20:37 +1200

    added offline support

--- database/Database.js
+++ database/Database.js
@@ -1,5 +1,27 @@
+// import * as SQLite from 'expo-sqlite/legacy';
+
+// export const DatabaseConnection = {
+//     getConnection: () => SQLite.openDatabase('database.db')
+// }
 import * as SQLite from 'expo-sqlite/legacy';
 
+const db = SQLite.openDatabase('database.db');
+
+// Create a table if it does not exist
+export const createTable = () => {
+  db.transaction(tx => {
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      );`
+    );
+  });
+};
+
+// Function to get the database connection
 export const DatabaseConnection = {
-    getConnection: () => SQLite.openDatabase('database.db')
-}
+  getConnection: () => db,
+  createTable: () => createTable(),
+};
